969801c772637b52cf059788ef2393ce67b596d3
whitecity01 <kh081104@gmail.com>
2026-06-04 17:48:13 +0900

    docs: 필요없는 문서 삭제

--- src/main/java/org/ssafy/ssarain/domain/user/dto/res/NameCheckRes.java
+++ src/main/java/org/ssafy/ssarain/domain/user/dto/res/NameCheckRes.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record NameCheckRes(
         @Schema(
                 requiredMode = Schema.RequiredMode.REQUIRED,
-                description = "닉네임 중복 여부 (true: 중복됨, false: 사용 가능)",
                 example = "true"
         )
         boolean isDuplicate
