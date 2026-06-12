b98204ad41be70638518d950b76915fb60f47ede
patternplane <bss011202@naver.com>
2026-06-12 11:11:52 +0900

    refact: 불필요 Getter 및 생성자 제거

--- src/main/java/org/ssafy/ssarain/domain/brain/model/BrainMemberRole.java
+++ src/main/java/org/ssafy/ssarain/domain/brain/model/BrainMemberRole.java
@@ -1,11 +1,5 @@
 package org.ssafy.ssarain.domain.brain.model;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum BrainMemberRole {
     USER,
     ADMIN
