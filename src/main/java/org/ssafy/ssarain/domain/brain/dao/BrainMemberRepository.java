69c79a8dfb1784f5425a0b8c4a17e50eb5bb83ad
patternplane <bss011202@naver.com>
2026-06-12 11:12:33 +0900

    refact: BrainAdmin DAO 쿼리 개선

--- src/main/java/org/ssafy/ssarain/domain/brain/dao/BrainMemberRepository.java
+++ src/main/java/org/ssafy/ssarain/domain/brain/dao/BrainMemberRepository.java
@@ -4,17 +4,14 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.ssafy.ssarain.domain.brain.model.BrainMember;
+import org.ssafy.ssarain.domain.brain.model.BrainMemberRole;
 
 public interface BrainMemberRepository extends JpaRepository<BrainMember, BrainMember.BrainMemberId> {
     
     List<BrainMember> findByBmid_Uid(UUID uid);
-    
-    @Query("SELECT (0 < count(*)) FROM BrainMember m WHERE m.bmid.uid = :uid AND role = 'ADMIN'")
-    boolean isAnyBrainAdmin(UUID uid);
-    
-    @Query("SELECT (0 < count(*)) FROM BrainMember m WHERE m.bmid.uid = :uid AND m.bmid.bid = :bid AND role = 'ADMIN'")
-    boolean isBrainAdminOf(UUID uid, int bid);
-    
+        
+    boolean existsByBmidUidAndRole(UUID uid, BrainMemberRole role);
+
+    boolean existsByBmidUidAndBmidBidAndRole(UUID uid, int bid, BrainMemberRole role);
 }
--- src/main/java/org/ssafy/ssarain/domain/brain/service/BrainAdminService.java
+++ src/main/java/org/ssafy/ssarain/domain/brain/service/BrainAdminService.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.ssafy.ssarain.domain.brain.dao.BrainMemberRepository;
+import org.ssafy.ssarain.domain.brain.model.BrainMemberRole;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,10 +15,10 @@ public class BrainAdminService {
     private final BrainMemberRepository brainMemberRepository;
     
     public boolean isAnyBrainAdmin(UUID uid) {
-        return brainMemberRepository.isAnyBrainAdmin(uid);
+        return brainMemberRepository.existsByBmidUidAndRole(uid, BrainMemberRole.ADMIN);
     }
     
     public boolean isBrainAdminOf(UUID uid, int bid) {
-        return brainMemberRepository.isBrainAdminOf(uid, bid);
+        return brainMemberRepository.existsByBmidUidAndBmidBidAndRole(uid, bid, BrainMemberRole.ADMIN);
     }
 }
