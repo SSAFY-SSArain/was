e06ca455bb2cc3a42bfb7315836c2e5b793cab61
whitecity01 <kh081104@gmail.com>
2026-06-14 15:06:25 +0900

    feat: title size 유효성 검사 추가

--- src/main/java/org/ssafy/ssarain/domain/node/dto/NodeCreateDto.java
+++ src/main/java/org/ssafy/ssarain/domain/node/dto/NodeCreateDto.java
@@ -4,13 +4,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.ssafy.ssarain.domain.node.model.Node;
-
-import java.util.UUID;
+import jakarta.validation.constraints.Size;
 
 public record NodeCreateDto(
 
         @Schema(requiredMode = RequiredMode.REQUIRED, example = "노드 제목 1")
+        @Size(max = 255)
         @NotBlank
         String title,
 
@@ -22,8 +21,4 @@ public record NodeCreateDto(
         @NotNull
         Integer btid
 ) {
-
-    public Node toEntity(UUID uid) {
-        return Node.of(btid,uid,title,content);
-    }
 }
