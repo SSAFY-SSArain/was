534ad44530cbf6b5bd06a422f4f480fb5a0f4022
Kim Seungje <122271925+whitecity01@users.noreply.github.com>
2026-06-24 14:21:37 +0900

    feat: brain 검색 시 본인 소속 여부, brain 가입 정책 반환 (#79)

--- src/main/java/org/ssafy/ssarain/domain/brain/dto/response/BrainFoundDto.java
+++ src/main/java/org/ssafy/ssarain/domain/brain/dto/response/BrainFoundDto.java
@@ -3,6 +3,8 @@ package org.ssafy.ssarain.domain.brain.dto.response;
 import java.util.List;
 
 import org.ssafy.ssarain.domain.brain.model.Brain;
+import org.ssafy.ssarain.domain.brain.model.JoinPolicy;
+import org.ssafy.ssarain.domain.brain.model.JoinStatus;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
@@ -19,12 +21,31 @@ public record BrainFoundDto(
         
         @Schema(requiredMode = RequiredMode.REQUIRED, example = "구미 캠퍼스 3반입니다.")
         String description,
+
+        @Schema(requiredMode = RequiredMode.REQUIRED, example = "PUBLIC")
+        JoinPolicy joinPolicy,
+
+        @Schema(requiredMode = RequiredMode.REQUIRED, example = "ACTIVE")
+        JoinStatus joinStatus,
         
         @Schema(requiredMode = RequiredMode.REQUIRED, example = "[ \"박성수\", \"백성수\" ]")
         List<String> memberNames
         ) {
     
-    public static BrainFoundDto from(Brain brain, String adminName, List<String> memberNames) {
-        return new BrainFoundDto(brain.getBid(), brain.getName(), adminName, brain.getDescription(), memberNames);
+    public static BrainFoundDto from(
+            Brain brain,
+            String adminName,
+            JoinStatus joinStatus,
+            List<String> memberNames
+    ) {
+        return new BrainFoundDto(
+                brain.getBid(),
+                brain.getName(),
+                adminName,
+                brain.getDescription(),
+                brain.getJoinPolicy(),
+                joinStatus,
+                memberNames
+        );
     }
 }
